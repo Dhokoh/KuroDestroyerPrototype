f0aee293ce445d010ae926ba0790b8c70485c3fa
Dhokoh <86420854+Dhokoh@users.noreply.github.com>
2021-07-29 23:13:45 +0200

    Update KuroController
    Edited and made code for method KuroSpotsEnemy() so that when player-shot raycast detects an enemy it would return true. Tried to check it with Debug.Logs but I was only able to see the boolean results there as the returns are not shown in the Console of Unity, but the right values show on Debug.Logs. 
    Added a LayerMask variable doing a bitshift.

--- KuroController.cs
+++ KuroController.cs
@@ -12,14 +12,15 @@ public class KuroController : MonoBehaviour
     public SpriteRenderer kuroSprite;
 
     public GameObject EnemyGameObject;
+    public BoxCollider2D EnemyCollider;
 
     float kuroMovSPD = 8f;
     float kuroJumpHgt = 8f;
 
     //Methods
 
-      public bool EnemyDetected() //Method defined to test if the raycast detects the Collider from the Enemy. 
-      {
+    public bool EnemyDetected() //Method defined to test if the raycast detects the Collider from the Enemy. 
+    {
         RaycastHit2D sight;
         int layerMask = 1 << 9;
         sight = Physics2D.Raycast(kuroSprite.transform.position, kuroSprite.transform.right, Mathf.Infinity, layerMask);
@@ -32,39 +33,46 @@ public class KuroController : MonoBehaviour
         {
             return false;
         }
-       }
+    }
 
-    public void KuroSpotsEnemy() //This method is supposed to be used for Kuro to "see" enemies and "react" to their sighting. 
-                                 //Still incomplete as I'm not able to turn it into a boolean to test the raycast in both the x axis
-                                 // and the -x axis.
+    public bool KuroSpotsEnemy() //This method is supposed to be used for Kuro to "see" enemies and "react" to their sighting. 
+    //Tried to check it with Debug.Logs but I was only able to see the boolean results there as the returns are not shown in the Console of Unity, 
+    //but the right values show on Debug.Logs.                              
     {
         RaycastHit2D kuroSight;
         int layerMask = 1 << 9;
-        kuroSight = Physics2D.Raycast(kuroSprite.transform.position, kuroSprite.transform.right, Mathf.Infinity, layerMask);
-        if (kuroSight.collider != null)
+        if (kuroSprite.flipX == false)
         {
+            kuroSight = Physics2D.Raycast(kuroSprite.transform.position, -kuroSprite.transform.right, Mathf.Infinity, layerMask);
             Debug.DrawRay(kuroSprite.transform.position, kuroSprite.transform.right, Color.blue);
-            Debug.Log(kuroSight.collider == EnemyCollider);
-            Debug.Log(kuroSight.collider);
-            //return true;
+
+            if (kuroSight.collider != null)
+            {
+                
+                Debug.Log(kuroSight.collider == EnemyCollider);
+                Debug.Log(kuroSight.collider);
+                return true;
+            }
         }
         if (kuroSprite.flipX == true)
         {
-            Debug.Log(kuroSprite.flipX);
             kuroSight = Physics2D.Raycast(kuroSprite.transform.position, -kuroSprite.transform.right, Mathf.Infinity, layerMask);
             Debug.DrawRay(kuroSprite.transform.position, -kuroSprite.transform.right, Color.yellow);
+            Debug.Log(kuroSprite.flipX);
             if (kuroSight.collider != null)
             {
-                Debug.DrawRay(kuroSprite.transform.position, -kuroSprite.transform.right, Color.yellow);
                 Debug.Log(kuroSight.collider == EnemyCollider);
                 Debug.Log(kuroSight.collider);
-                //return true;
+                return true;
             }
         }
-        //return false;
+        return false;
     }
+    
+
     public void MoveKuro()
     {
+        SpriteRenderer kuroSprite = this.GetComponentInChildren<SpriteRenderer>();
         //attempt of Transform.Translate()
         if (Input.GetAxis("Horizontal") == 1 | Input.GetKeyDown(KeyCode.D))
         {
@@ -79,7 +87,7 @@ public class KuroController : MonoBehaviour
             KuroControllerXform.Translate(kuroMoves * kuroMovSPD * Time.deltaTime,0,0);
         }
     }
-    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -89,19 +97,27 @@ public class KuroController : MonoBehaviour
         kuroAnimator = this.gameObject.GetComponentInChildren<Animator>();
 
         EnemyGameObject = GameObject.FindObjectOfType<EnemyController>().gameObject;
+        EnemyCollider = EnemyGameObject.GetComponentInChildren<BoxCollider2D>();
     }
 
     //Update is called once per frame
     void Update()
     {
         float kuroJump = 0f;
+        bool enemySpotted = KuroSpotsEnemy();
         //Attempt to move with Translate
         MoveKuro();
+
+        if (enemySpotted == true)
+        {
+            kuroAnimator.SetBool("EnemySpotted", enemySpotted);
+        }
+        kuroAnimator.SetBool("EnemySpotted", enemySpotted);
         
         //Code for jumping. *****Pro tip> if player holds Space for 1,5 seconds, Kuro will do longer jumps... levitation :3. It is even
         //possible to perform a jump-attack (basic animation of attacking while jumping). Lucky shot xD, but now I know <3. 
         kuroJump = Input.GetAxis("Jump");
-        Debug.Log(kuroJump);
+        //Debug.Log(kuroJump);
         KuroControllerXform.Translate(0, kuroJump * kuroJumpHgt * Time.deltaTime, 0);
         
         //Code for attacking. Very basic really. Setting triggers on animations. I think it's the "best option" but well, I'm a noob. So maybe there's
@@ -112,8 +128,8 @@ public class KuroController : MonoBehaviour
         }
         kuroAnimator.SetTrigger("Non-Attack");
 
-        //Attempts to make Kuro's vision range... using a Raycast... effing thing to be so hard to grasp!!! 
-        EnemyDetected(); //1.Testing if a hit is made. And if the ray is being casted.   
-
+        //KuroRaycastTest();
+        //Attempts to make Kuro's vision range... using a Raycast... Hard to grasp!!! 
+        //KuroSpotsEnemy(); //1.Testing if a hit is made. And if the ray is being casted.   
     }
 } 
