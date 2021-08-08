dfbe3d7d1efc4166059eaa2fc6ed3ed441928a05
Dhokoh <86420854+Dhokoh@users.noreply.github.com>
2021-08-08 21:55:20 +0200

    Added script defining the Gem class
    Gem is a basic class, with its own parameters. 
    Gems will be of definitive values being = 1, 10, 100, 1000. 
    Color coded. Colors still unknown.

---
+++ Gem.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gem : MonoBehaviour
+{
+    //Gems will be 1-10-100-1000 respectively.
+    //Yellow Gems = 1
+    //
+    public int value; 
+    public bool isTaken = false;
+    // Start is called before the first frame update
+    
+}
