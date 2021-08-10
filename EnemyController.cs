18cc7a158b83aa9c56e21a27d3c6bd1a01c2297c
Dhokoh <86420854+Dhokoh@users.noreply.github.com>
2021-08-10 19:18:40 +0200

    Updated EnemyController
    Redo of the class. Redefined attributes and attempting to successfully code the enemy movement "patrolling".

--- EnemyController.cs
+++ EnemyController.cs
@@ -4,66 +4,44 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    public float enemyWalkSpeed = 3f;
+    /// <summary>
+    /// EnemyController is a class that controls basic information and movements from enemies, including bosses and minibosses.
+    /// Consider Enemies HP, Damage, movement and "Kuro detection". 
+    /// </summary>
+    public float enemyHP;//this is to be assigned via the Unity Editor. Not all enemies have the same HP.
+    public float enemyDMG;// this is to be assigned via de Unity Editor. Not all enemies deal the same damage. 
+    public float enemyWalkSpd;//This is to be assigned via de Unity Editor. Not all enemies walk equally fast. 
+    public bool isBoss; //this is to be assigned via the Unity Editor. Boss class will be created later on and will interact with this one. 
 
-    public GameObject EnemyControllerGameObject;
-    public BoxCollider2D EnemyCollider;
-    public SpriteRenderer enemySprite;
-    
-    public GameObject KuroControllerGameObject;
-    public KuroController kuroCtrl;
-    public BoxCollider2D kuroCollider;
+    public GameObject EnemyControllerGO;
+    public RaycastHit2D enemySight;
 
-    //Methods
-
-    //this one is to test how raycast works on this with A MERE FUNCTION TO CAST A RAY AND WITH DEBUG.LOG SEE IT ON THE EDITOR. 
-    public void RaycastTest()
-    {
-        int playerLayermask = 1 << 8;
-        RaycastHit2D enemySight = Physics2D.Raycast(enemySprite.transform.position, enemySprite.transform.right, Mathf.Infinity, playerLayermask);
-        Debug.DrawRay(enemySprite.transform.position, -enemySprite.transform.right, Color.green);
-        if (enemySight.collider != null)
-        {
-            Debug.Log(enemySight.collider);
-            Debug.Log(enemySight.collider == kuroCollider);
-            //return true;
-        }
-        else
-        {
-            Debug.Log(enemySight.collider);
-            Debug.Log(enemySight.collider == kuroCollider);
-            //return false
-        }
-        
-    }
-
-    //boolpublic bool PlayerDetected()
-    public void MoveEnemy()   
-    {
-        while (EnemyControllerGameObject.transform.position.x <= enemyWalkSpeed)
-        {
-            EnemyControllerGameObject.transform.Translate(enemyWalkSpeed * Time.deltaTime, 0, 0);
-        }
-        enemySprite.flipX=true; 
-    }
 
     // Start is called before the first frame update
     void Start()
     {
-        KuroControllerGameObject = GameObject.FindGameObjectWithTag("Player");
-        kuroCollider = KuroControllerGameObject.GetComponent<BoxCollider2D>();
-        kuroCtrl = GameObject.FindObjectOfType<KuroController>();
-
-        EnemyControllerGameObject = GameObject.FindGameObjectWithTag("Enemy");
-        enemySprite = this.GetComponentInChildren<SpriteRenderer>();
-        EnemyCollider = this.GetComponent<BoxCollider2D>();
+        EnemyControllerGO = this.gameObject;
     }
+
+
+    //Private class methods
+
+    private void EnemyMovement()
+    {
+        EnemyControllerGO.GetComponent<Rigidbody2D>().velocity = new Vector2(1,0);
+        EnemyControllerGO.GetComponent<Rigidbody2D>().AddForce(new Vector2(1, 0));
+    }
+
+
+
+
+
 
 
     //Update is called once per frame
     void Update()
     {
-        MoveEnemy();
-        RaycastTest();
+        EnemyMovement();
+        Debug.Log(EnemyControllerGO.transform.position);
     }
 }
