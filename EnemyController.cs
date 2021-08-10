8bf136cc544c02ca6cb9c1ae11bc2501d871a3fc
Dhokoh <86420854+Dhokoh@users.noreply.github.com>
2021-08-11 00:30:59 +0200

    Updated EnemyController
    Now the enemy moves but only in one direction (to the left)
    There's a new method called TurnAround() that uses the localScale from the transform to turn or flip the sprite. Need to get deeper understanding on this localScale thing.

--- EnemyController.cs
+++ EnemyController.cs
@@ -11,26 +11,45 @@ public class EnemyController : MonoBehaviour
     public float enemyHP;//this is to be assigned via the Unity Editor. Not all enemies have the same HP.
     public float enemyDMG;// this is to be assigned via de Unity Editor. Not all enemies deal the same damage. 
     public float enemyWalkSpd;//This is to be assigned via de Unity Editor. Not all enemies walk equally fast. 
-    public bool isBoss; //this is to be assigned via the Unity Editor. Boss class will be created later on and will interact with this one. 
+    public float moveUnits = 5;
+    public float initialPosX;
+
 
     public GameObject EnemyControllerGO;
+    public SpriteRenderer enemySprite;
     public RaycastHit2D enemySight;
 
+    public bool isFacingRight;
+    public bool isBoss; //this is to be assigned via the Unity Editor. Boss class will be created later on and will interact with this one. 
 
     // Start is called before the first frame update
     void Start()
     {
         EnemyControllerGO = this.gameObject;
+        enemySprite = EnemyControllerGO.GetComponentInChildren<SpriteRenderer>();
+        isFacingRight = EnemyControllerGO.transform.localScale.x > 0;
+        initialPosX = EnemyControllerGO.transform.position.x;
+        Debug.Log(EnemyControllerGO.transform.localScale.x);
+        Debug.Log(isFacingRight);
     }
 
 
     //Private class methods
 
-    private void EnemyMovement()
+    private void TurnAround()
     {
-        EnemyControllerGO.GetComponent<Rigidbody2D>().velocity = new Vector2(1,0);
-        EnemyControllerGO.GetComponent<Rigidbody2D>().AddForce(new Vector2(1, 0));
+        transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+        isFacingRight = EnemyControllerGO.transform.localScale.x > 0;
+        Debug.Log(transform.localScale);
+        Debug.Log(isFacingRight);
     }
+
+    private void EnemyPatrol()
+    {
+        EnemyControllerGO.transform.Translate(-Vector2.right * enemyWalkSpd * Time.deltaTime);
+        Debug.Log(EnemyControllerGO.transform.position);
+    }
+   
 
 
 
@@ -41,7 +60,6 @@ public class EnemyController : MonoBehaviour
     //Update is called once per frame
     void Update()
     {
-        EnemyMovement();
-        Debug.Log(EnemyControllerGO.transform.position);
+        EnemyPatrol();
     }
 }
